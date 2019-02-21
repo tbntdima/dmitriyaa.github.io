f430a6351204d9c9896d661e727af9e5e2535bfb
Dmitriy An <an.dmitriy01@gmail.com>
2019-02-21 23:32:45 +0100

    Updates

---
+++ precache-manifest.da659769584d14759964dc9305dde408.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "05a83a782c9d8ae0480a",
+    "url": "/static/css/main.acce7bf1.chunk.css"
+  },
+  {
+    "revision": "05a83a782c9d8ae0480a",
+    "url": "/static/js/main.05a83a78.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "334a55bc64c520b7a1351bc51ca1a337",
+    "url": "/index.html"
+  }
+];
--- readme.md
+++
@@ -1,1 +0,0 @@
-# dmitriyaa.github.io
