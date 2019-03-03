317211fd0a0047bea819350db6990bd56bbb681b
Dmitriy An <an.dmitriy01@gmail.com>
2019-03-03 12:28:44 +0100

    switched to HistoryRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 // Custom components
 import App from './components/App/';
