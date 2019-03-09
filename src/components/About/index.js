963a643b51c2d0297437984bdba6857b3683733f
Dmitriy An <an.dmitriy01@gmail.com>
2019-03-09 22:09:24 +0100

    updated About description

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,7 +12,10 @@ class About extends Component {
       <div className="about">
         <h1 className="color--accent">I'm Dmitriy An</h1>
         <p>Professional <span>Front-End Web Developer</span>, who is building beautifull, clean and modern websites. With use of latest web technologies.</p>
-        <p>I love <span>JavaScript</span>, and currently focused on <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> library and its ecosystem. But although I mainly specialize in <span>Front-End</span>, I have some experience with <span>Back-End</span> as well <a href="https://laravel.com/" target="_blank" rel="noopener noreferrer">(Laravel)</a>.</p>
+        <p>
+          I love <span>JavaScript</span>, and currently focused on <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> library and its ecosystem.
+          <br/>Although I mainly specialize in <span>Front-End</span>, I have some experience with <span>Back-End</span> as well.
+        </p>
       </div>
     );
   }
