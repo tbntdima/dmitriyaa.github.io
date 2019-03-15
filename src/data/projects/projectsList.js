49499450841d64fe3a09cbb318e102454c5e0417
Dmitriy An <an.dmitriy01@gmail.com>
2019-03-15 13:47:41 +0100

    added flip a coin

--- src/data/projects/projectsList.js
+++ src/data/projects/projectsList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import mob from './images/mp100.jpg';
+import fac_m from './images/fac-m.jpg';
+import fac_d from './images/fac-d.jpg';
 import wad_m from './images/wad-m.jpg';
 import wad_d from './images/wad-d.jpg';
 import avtoskola_m from './images/avtoskola-m.jpg';
@@ -25,6 +27,21 @@ const projectsList = [
     ),
     img_mobile: mob,
     img_desktop: '',
+    completion_date: '2019'
+  },
+  {
+    id: 'Flip a Coin Game',
+    title: 'Flip a Coin Game',
+    tag: 'Personal',
+    url_live: 'https://www.p7campaign.com/landing/flip-a-coin-w1/',
+    url_git: 'https://github.com/dmitriyaa/react-flip-a-coin-game',
+    description: (
+      <div>
+        <p>An interactive game-like landing page built on react.</p>
+      </div>
+    ),
+    img_mobile: fac_m,
+    img_desktop: fac_d,
     completion_date: '2019'
   },
   {
