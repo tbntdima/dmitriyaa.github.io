7a1a42b15fd1fd6a52a90b7184e5ef840abc6a4b
Dmitriy An <an.dmitriy01@gmail.com>
2019-05-27 00:19:50 +0200

    first attempt to implement blog section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,21 @@
 import React from "react"
 import { Box } from "@rebass/grid"
+import { withTheme } from "styled-components"
+import { Link, graphql } from "gatsby"
 
+import { formatReadingTime } from "../utils/helpers"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 import Bio from "../components/Bio"
 import AnimatedCollapse from "../components/AnimatedCollapse"
+import { rhythm } from "../theme/typography"
 
 class Homepage extends React.Component {
   render() {
+    const { theme, data } = this.props
+    const posts = data.allMarkdownRemark.edges
     const hash = this.props.location.hash
+
     return (
       <Layout>
         <SEO title="" />
@@ -16,6 +23,7 @@ class Homepage extends React.Component {
           style={{
             maxWidth: "540px",
           }}
+          mb={rhythm(1.25)}
         >
           <Bio />
         </Box>
@@ -23,9 +31,42 @@ class Homepage extends React.Component {
         <AnimatedCollapse title="Projects" currentHash={hash}>
           <p>projects</p>
         </AnimatedCollapse>
+
         <AnimatedCollapse title="Blog" currentHash={hash}>
-          <p>blog</p>
+          <div>
+            {posts.map(({ node }) => {
+              const title = node.frontmatter.title || node.fields.slug
+              return (
+                <article key={node.fields.slug}>
+                  <Link to={node.fields.slug}>
+                    <h3
+                      style={{
+                        marginTop: rhythm(1 / 2),
+                        marginBottom: 0,
+                        paddingBottom: rhythm(1 / 4),
+                      }}
+                    >
+                      {title}
+                    </h3>
+                  </Link>
+                  <small style={{ color: theme.colorAccent }}>
+                    {node.frontmatter.date}
+                    {` • ${formatReadingTime(node.timeToRead)}`}
+                  </small>
+                  <p
+                    style={{
+                      marginTop: rhythm(1 / 4),
+                    }}
+                    dangerouslySetInnerHTML={{
+                      __html: node.frontmatter.description || node.excerpt,
+                    }}
+                  />
+                </article>
+              )
+            })}
+          </div>
         </AnimatedCollapse>
+
         <AnimatedCollapse title="Contact" currentHash={hash}>
           <p>Contacts</p>
         </AnimatedCollapse>
@@ -34,4 +75,25 @@ class Homepage extends React.Component {
   }
 }
 
-export default Homepage
+export default withTheme(Homepage)
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          timeToRead
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
