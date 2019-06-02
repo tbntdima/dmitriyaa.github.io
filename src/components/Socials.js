c0dd83ea1ae9bb6817656884f326377c953ff751
Dmitriy An <an.dmitriy01@gmail.com>
2019-06-03 00:12:15 +0200

    wip: socials component

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,23 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 
-const Socials = ({ direction }) => {
-  return <div />
+const Socials = ({ direction, data }) => {
+  return <div>{data.site.siteMetadata.social.twitter}</div>
 }
 
-export default Socials
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            social {
+              twitter
+            }
+          }
+        }
+      }
+    `}
+    render={data => <Socials data={data} {...props} />}
+  />
+)
